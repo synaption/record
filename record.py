47e05a5901f9b0de3f29b7d51814e1b3cc4a9dee
bob mcgrath <bobwmcgrath@gmail.com>
2024-03-10 19:24:38 -0500

    even more minimal record.py
    will this crash?

--- record.py
+++ record.py
@@ -1,23 +1,12 @@
-import time
 import alsaaudio
-import traceback
 
-if __name__ == '__main__':
+device = 'dmic_sv'
 
-    device = 'dmic_sv'
+inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NONBLOCK, 
+    channels=2, rate=32000, format=alsaaudio.PCM_FORMAT_S32_LE, 
+    periodsize=640, device=device)
+    
+while True:
+    # Read data from device
+    inp.read()
 
-    inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NONBLOCK, 
-        channels=2, rate=32000, format=alsaaudio.PCM_FORMAT_S32_LE, 
-        periodsize=640, device=device)
-
-
-    while True:
-        try:
-            # Read data from device
-            l, data = inp.read()
-            time.sleep(.001)
-            
-                
-        except Exception as e:  # Catch the exception and print the traceback
-            traceback.print_exc()
-            
